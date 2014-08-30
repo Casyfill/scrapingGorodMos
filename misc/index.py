fffe66ba0afd75080e88c0c056ad565a76e4039b
Casyfill <casyfill@gmail.com>
2014-08-30 18:22:53 +0400

    minor

---
+++ misc/index.py
@@ -0,0 +1,11 @@
+
+
+zonecodes = [{"name":"ЗАО","index":19},
+	{"name":"ЮЗАО","index":101},
+	{"name":"ЮАО","index":114},
+	{"name":"ЮВАО","index":88},
+	{"name":"ВАО","index":1},
+	{"name":"СВАО","index":50},
+	{"name":"САО","index":33},
+	{"name":"СЗАО","index":68},
+	{"name":"ЗелАО","index":131}]
